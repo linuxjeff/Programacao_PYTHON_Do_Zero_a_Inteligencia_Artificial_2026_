c7f2a9cac56497116fed737346f46ebc00b0298c
jefferson Santana <computadores.rp@gmail.com>
2026-09-22 18:34:00 -0300

    Aula 42

---
+++ 42. Modificando o tipo de dados.py
@@ -0,0 +1,13 @@
+# Como motificação de dado.
+
+x = str(3)
+y = int(4)
+z = float(5)
+
+print(f'{type(x)} = {x}')
+print(f'{type(y)} = {y}')
+print(f'{type(z)} = {z}')
+print('\n')
+print(x + x)
+print(y + y)
+print(z + z)
