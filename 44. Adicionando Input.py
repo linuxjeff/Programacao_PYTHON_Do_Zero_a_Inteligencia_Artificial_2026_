f492707f6b246f2a1b3cdf24b7f8df9ac53049ca
jefferson Santana <computadores.rp@gmail.com>
2026-09-25 18:41:02 -0300

    Aula 44

---
+++ 44. Adicionando Input.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome? \n>>> ')
+idade = input('Qual é a sua idade? \n>>> ')
+cidade = input('Qual é a sua cidade? \n>>> ')
+
+print(f'O {nome} tem {idade} anos e mora em {cidade}.')
