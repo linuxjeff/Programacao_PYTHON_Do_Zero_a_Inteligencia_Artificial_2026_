66ae9af17ad3a89a4adb01c2743b22aa16c8a723
jefferson Santana <computadores.rp@gmail.com>
2026-09-26 08:38:34 -0300

    Aula 47

---
+++ 47. Utilizando Formated Strings.py
@@ -0,0 +1,7 @@
+# Facilita a leitura da string.
+# O Marcos Silva é um excelente [Programador]
+nome = input('Digite seu nome:\n>>> ')
+sobrenome = input('Digite seu sobrenome:\n>>> ')
+profissao = input("Digite seu profição:\n>>> ")
+
+print(f'O {nome} {sobrenome} é um exelente [{profissao}].')
